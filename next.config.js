fa098f286dbb746f52894f10bb477054bc8d3b03
spkillaz <elan.savinon@gmail.com>
2022-05-23 16:59:07 -0400

    env

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_MARKETPLACE_CONTRACT_ADDRESS:
-      "0x57645d225490558AACDb60E2503d8daC55AE9D0b",
+      "0x34d93E2f645bDe7773e0Ffa40d2454B0A5baEbcC",
     NFT_COLECTION_CONTRACT_ADDRESS:
-      "0xFe1d218b269D5f202961d1C6F72C0101ad10848c",
+      "0x739A3E33fBd1Ce9f88b4e2588445818485730Ddc",
     MORALIS_SERVER_URL:
       "https://mixtowg6gzx7.usemoralis.com:2053/server",
     MORALIS_APP_ID:
       "rP7p98Lr9FFmhiQEX9GsZoMRlQA6QwHqVvkCdHRI",
     MORALIS_MASTER_KEY:
       "EWj2dIJWjzW7BzqD1Qnv90idnTBjXfbB8eVRJO0k",
+    CLOUDINARY_PRESET:
+      "cryptozon-prod",
   },
   async redirects() {
     return [
