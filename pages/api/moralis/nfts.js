97ff9c960ccc623a4b84fac6f054055c34332ee3
spkillaz <elan.savinon@gmail.com>
2022-05-23 17:44:43 -0400

    wrapped moralis stuff in try

--- pages/api/moralis/nfts.js
+++ pages/api/moralis/nfts.js
@@ -16,31 +16,36 @@ export default async function handler(req,res){
             await Moralis.start({ serverUrl, appId, masterKey });
             const ownedNFTs = await Moralis.Web3API.account.getNFTs({address:query.address, chain:'rinkeby'})
             const proccessNfts = await Promise.all(ownedNFTs.result.map(async (nftData,ind) =>{
-                const nftsInDB = await NFTs.findAll({where:{uri:`ipfs://${nftData.token_uri.split('').slice(34).join('')}`}});
-                const metadata = JSON.parse(nftData.metadata)
-                const imageIPFT = metadata.image.split('').slice(7).join('')
-                const imageUrl = `https://cryptozon.infura-ipfs.io/ipfs/${imageIPFT}`
-                if(nftsInDB.length>0){
-                    console.log('found nft in db using that info')
-                }else{
-                    const newNft = {
-                        owner: query.address,
-                        name: metadata.name,
-                        description: metadata.description,
-                        tokenId: nftData.token_id,
-                        image: imageUrl,
-                        uri: `ipfs://${nftData.token_uri.split('').slice(34).join('')}`,
-                        assetContractAddress: nftData.token_address,
-                        listingId: null,
-                        buyoutPrice: null,
-                        expirationDate: null,
-                        hidden: false,
-                        collectionId: null,
-                        createdAt: nftData.synced_at,
+                try {
+                    const nftsInDB = await NFTs.findAll({where:{uri:`ipfs://${nftData.token_uri.split('').slice(34).join('')}`}});
+                    const metadata = JSON.parse(nftData.metadata)
+                    const imageIPFT = metadata.image.split('').slice(7).join('')
+                    const imageUrl = `https://cryptozon.infura-ipfs.io/ipfs/${imageIPFT}`
+                    if(nftsInDB.length>0){
+                        console.log('found nft in db using that info')
+                    }else{
+                        const newNft = {
+                            owner: query.address,
+                            name: metadata.name,
+                            description: metadata.description,
+                            tokenId: nftData.token_id,
+                            image: imageUrl,
+                            uri: `ipfs://${nftData.token_uri.split('').slice(34).join('')}`,
+                            assetContractAddress: nftData.token_address,
+                            listingId: null,
+                            buyoutPrice: null,
+                            expirationDate: null,
+                            hidden: false,
+                            collectionId: null,
+                            createdAt: nftData.synced_at,
+                        }
+                        await NFTs.create(newNft);
+                        return 'success'
                     }
-                    await NFTs.create(newNft);
-                    return 'success'
+                } catch (error) {
+                    console.log(error)
                 }
+                
             }))
             console.log(proccessNfts.length," nfts found")
             const ownerNFTs = await NFTs.findAll({ where: { owner: query.address } });
